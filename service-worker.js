b4b5230ccca314d46c6e8ef8ed1f650f93687bea
ZOHAR VOLOKITA <88446440+zaselio@users.noreply.github.com>
2022-12-10 12:37:45 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+﻿var cacheName = "geolocation-cache-v2"
+
+var assets = [
+    "./",
+    "index.html",
+    "css/style.css",
+    "css/bootstrap.rtl.min.css",
+    "js/main.js",
+    "js/bootstrap.bundle.js",
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(cacheName).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
