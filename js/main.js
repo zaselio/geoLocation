bb1de723ad8ad0d425a2d467078b274d4a854c7a
ZOHAR VOLOKITA <88446440+zaselio@users.noreply.github.com>
2022-12-10 12:43:52 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+﻿window.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("find-me").addEventListener("click",geoFindMe);
+    document.getElementById("shareBtn").addEventListener("click",share);
+
+    var linkToShare="";
+    var titleToShare="";
+    
+    function geoFindMe(){
+        //console.log("geoFindMe");
+        if ('geolocation' in navigator) {
+            document.getElementById("status").textContent="מאתר את מיקומך";
+
+            navigator.geolocation.getCurrentPosition(success,error);
+
+          } else {
+            document.getElementById("status").textContent="הדפדפן לא תומך באיתור מיקום";
+          }          
+    }
+    function success(position){
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        linkToShare = `https://maps.google.com/?q=${latitude},${longitude}`;
+        titleToShare = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+
+        document.getElementById("map-link").textContent=titleToShare;
+        document.getElementById("iframe").src=linkToShare;
+        document.getElementById("iframe").classList.remove("d-none");
+    }
+
+    function error(){
+        document.getElementById("status").textContent="הדפדפן לא הצליח לאחזר את מיקומך";
+    }
+
+    function share(){
+        if(navigator.share){
+            const shareData ={
+                title:"my Geo Location",
+                text:titleToShare,
+                ur:linkToShare,
+            }
+            navigator.share(shareData);
+        }
+        else{
+            document.getElementById("status").textContent="הדפדפן לא הצליח לשתף את מיקומך";
+        }
+    }
+})
